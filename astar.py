30f575aab6a2069506360cef899b4dcae3aff40d
LouieW <dustyfingers01257@gmail.com>
2022-02-13 15:22:07 -0700

    scaffolded out spot class (node) and built grid display functions

--- astar.py
+++ astar.py
@@ -17,6 +17,182 @@ ORANGE = (255, 165, 0)
 GREY = (128, 128, 128)
 TURQUOISE = (64, 224, 208)
 
-# this is a node, essentially
+
+# this class represents a node, essentially
 class Spot:
     
+    # constructor 
+    
+    def __init__(self, row, col, width, total_rows):
+        self.row = row
+        self.col = col
+        self .x  = row * width
+        self.y  = col * width
+        self.color = WHITE
+        self.neighbors = []
+        self.width = width
+        self.total_rows = total_rows
+    
+    # checker methods...not really getters per se 
+    
+    def get_pos(self):
+        return self.row, self.col
+    
+    def is_closed(self):
+        return self.color == RED
+    
+    def is_open(self):
+        return self.color == GREEN
+    
+    def is_barrier(self):
+        return self.color == BLACK
+    
+    def is_start(self):
+        return self.color == ORANGE
+    
+    def is_end(self):
+        return self.color == TURQUOISE
+    
+    # setter methods 
+    
+    def make_closed(self):
+        self.color = RED
+    
+    def make_open(self):
+        self.color = GREEN
+    
+    def make_barrier(self):
+        self.color = BLACK
+        
+    def make_start(self):
+        self.color = ORANGE
+    
+    def make_end(self):
+        self.color = TURQUOISE
+        
+    def make_path(self):
+        self.color = PURPLE
+        
+    # general utils
+    
+    def reset(self):
+        self.color == WHITE
+        
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
+       
+    # TODO: finish this later 
+    def update_neighbors(self):
+        pass
+        
+    # give this class a behavior when we use the less than function on it
+    def __lt__(self, other):
+        return False
+# SPOT CLASS END
+    
+
+# simple manhattan or taxicab distance function
+def distance_function(p1, p2):
+    # split the variables into their components
+    x1, y1 = p1
+    x2, y2 = p2
+    
+    return abs(x1 - x2) + abs(y1 - y2)
+        
+
+# this function constructs our grid of Spot instances
+def make_grid(rows, width):
+    grid = []
+    gap = width // rows # integer division
+    
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            # no new keyrowd in python!
+            spot = Spot(i, j , gap, rows)
+            grid[i].append(spot)
+    
+    return grid
+
+
+def draw_spot_grid(win, grid):
+    for row in grid:
+        for spot in row:
+            spot.draw(win)
+
+
+def draw_grid_lines(win, rows, width):
+    gap = width // rows
+    
+    for i in range(rows):
+        pygame.draw.line(win, GREY, (0, i * gap), (width, i * gap))
+        for j in range(rows):
+            pygame.draw.line(win, GREY, (j * gap, 0), (j * gap, width))
+  
+            
+def draw(win, grid, rows, width):
+    win.fill(WHITE)
+    
+    draw_spot_grid(win, grid)
+
+    draw_grid_lines(win, rows, width)
+    
+    pygame.display.update()
+    
+    
+def get_clicked_pos(pos, rows, width):
+    gap = width // rows
+    
+    y, x = pos
+    
+    row = y // gap
+    col = x // gap
+    
+    return row, col
+
+
+def main(win, width):
+    ROWS = 50
+    grid = make_grid(ROWS, width)
+    
+    start = None
+    end = None
+    
+    run = True
+    started = False
+    
+    while run:
+        draw(win, grid, ROWS, width)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                
+            if started:
+                continue
+            
+            # handle left mouse button clicking
+            if pygame.mouse.get_pressed()[0]:
+                pos = pygame.mouse.get_pos()
+                row, col = get_clicked_pos(pos, ROWS, width)
+                
+                spot = grid[row][col]
+                
+                if not start and spot != end:
+                    start = spot
+                    start.make_start()
+                    
+                elif not end and spot != start:
+                    end = spot 
+                    end.make_end()
+                    
+                elif spot != end and spot != start:
+                    spot.make_barrier()  
+                
+            # handle right mouse button clicking
+            elif pygame.mouse.get_pressed()[2]:
+                pass
+                  
+    pygame.quit()
+
+
+main(WIN, WIDTH)
